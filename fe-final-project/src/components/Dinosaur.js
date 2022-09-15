2af5f54d263edefb14fd644c7bb5b707f4c39f0a
spobrien1116 <87784429+spobrien1116@users.noreply.github.com>
2022-09-15 17:38:38 -0400

    Post-mentor session
    Went over design potentials for the project. Currently trying to add images to the dinosaurs. Updated the API's information to reflect this change. Directly changed the schema of a dinosaur in the API as well.

--- fe-final-project/src/components/Dinosaur.js
+++ fe-final-project/src/components/Dinosaur.js
@@ -1,48 +1,74 @@
-import React from 'react';
-import {NewFeatureForm} from './NewFeatureForm';
+import React from "react";
+import { NewFeatureForm } from "./NewFeatureForm";
+import TRex from "../images/sad-dino.jpg";
 
 export const Dinosaur = (props) => {
-    const {dinosaur, updateDinosaur, deleteDinosaur} = props;
+  const { dinosaur, updateDinosaur, deleteDinosaur } = props;
 
-    const deleteFeature = (index) => {
-        console.log("A feature is being deleted!");
-        const updatedDinosaur = {
-            ...dinosaur,
-            features: dinosaur.features.filter((x) => dinosaur.features.indexOf(x) !== index)
-        };
-        updateDinosaur(updatedDinosaur);
+  const deleteFeature = (index) => {
+    console.log("A feature is being deleted!");
+    const updatedDinosaur = {
+      ...dinosaur,
+      features: dinosaur.features.filter(
+        (x) => dinosaur.features.indexOf(x) !== index
+      ),
     };
+    updateDinosaur(updatedDinosaur);
+  };
 
-    const deleteDinosaurByID = (dinosaurID) => {
-        deleteDinosaur(dinosaurID);
-    };
+  const deleteDinosaurByID = (dinosaurID) => {
+    deleteDinosaur(dinosaurID);
+  };
 
-    const addNewFeature = (feature) => updateDinosaur({...dinosaur, features: [...dinosaur.features, feature]});
+  const addNewFeature = (feature) =>
+    updateDinosaur({ ...dinosaur, features: [...dinosaur.features, feature] });
 
-    const features = () => (
-        <ul className="list-group list-group-flush dinoFeatures">
-            {dinosaur.features.map((feature, index) => (
-                <li key={index} className="list-group-item">
-                    <b>Body Part: </b>{`${feature.part}`}   <b>Color: </b>{`${feature.color}`}
-                    <button className="btn btn-danger featureDeleteButton" onClick={(e) => deleteFeature(index)}>Delete Feature</button>
-                </li>
-            ))}
-        </ul>
-    );
+  const features = () => (
+    <ul className="list-group list-group-flush dinoFeatures">
+      {dinosaur.features.map((feature, index) => (
+        <li key={index} className="list-group-item">
+          <b>Body Part: </b>
+          {`${feature.part}`} <b>Color: </b>
+          {`${feature.color}`}
+          <button
+            className="btn btn-danger featureDeleteButton"
+            onClick={(e) => deleteFeature(index)}
+          >
+            Delete Feature
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 
-    return (
-        <div className="card p-4 m-2" key={dinosaur._id}>
-            <div className="card-header">
-                <h3 className="cardDinoName">{dinosaur.name}</h3>
-                <button className="btn btn-danger deleteDinosaurButton" onClick={(e) => deleteDinosaurByID(dinosaur._id)}>Delete Dinosaur</button>
-            </div>
-            <ul className="list-group list-group-flush dinoInfo">
-                <li className="list-group-item"><b>Size: </b>{`${dinosaur.size}`}</li>
-                <li className="list-group-item"><b>Diet: </b>{`${dinosaur.diet}`}</li>
-                <li className="list-group-item"><b>Environment: </b>{`${dinosaur.environment}`}</li>
-            </ul>
-            {features({features, dinosaurID: dinosaur._id, deleteFeature})}
-            <NewFeatureForm addNewFeature={addNewFeature} />
-        </div>
-    )
+  return (
+    <div className="card p-4 m-2" key={dinosaur._id}>
+      <div className="card-header">
+        <h3 className="cardDinoName">{dinosaur.name}</h3>
+        <button
+          className="btn btn-danger deleteDinosaurButton"
+          onClick={(e) => deleteDinosaurByID(dinosaur._id)}
+        >
+          Delete Dinosaur
+        </button>
+      </div>
+      <ul className="list-group list-group-flush dinoInfo">
+        <li className="list-group-item">
+          <b>Size: </b>
+          {`${dinosaur.size}`}
+        </li>
+        <li className="list-group-item">
+          <b>Diet: </b>
+          {`${dinosaur.diet}`}
+        </li>
+        <li className="list-group-item">
+          <b>Environment: </b>
+          {`${dinosaur.environment}`}
+        </li>
+      </ul>
+      {features({ features, dinosaurID: dinosaur._id, deleteFeature })}
+      <NewFeatureForm addNewFeature={addNewFeature} />
+      <img src={dinosaur.image} alt="This is the dinosaur." />
+    </div>
+  );
 };
--- fe-final-project/src/components/NewDinosaurForm.js
+++ fe-final-project/src/components/NewDinosaurForm.js
@@ -1,52 +1,114 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
+import TRex from "../images/sad-dino.jpg";
 
 export const NewDinosaurForm = (props) => {
-    const [name, setName] = useState('');
-    const [size, setSize] = useState('');
-    const [diet, setDiet] = useState('');
-    const [environment, setEnvironment] = useState('');
-    
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (name && size && diet && environment) {
-            props.addNewDinosaur({name, size, diet, environment});
-            setName('');
-            setSize('');
-            setDiet('');
-            setEnvironment('');
-            console.log("A new dinosaur is being created!");
-        } else {
-            console.log("Do not submit a blank value. Please enter a string for the dinosaur's name, size, diet, and environment.");
-        }
-    };
+  const [name, setName] = useState("");
+  const [size, setSize] = useState("");
+  const [diet, setDiet] = useState("");
+  const [environment, setEnvironment] = useState("");
+  const [image, setImage] = useState("");
 
-    return (
-        <div>
-            <form className="container border border-success p-4" id="dinoForm" onSubmit={onSubmit}>
-                <div className="form-group mb-3">
-                    <label htmlFor="place" className="form-control dinoFormLabel">Enter your new dinosaur name.</label>
-                    <input type="text" className="form-control dinoFormInput" id="dinoName" placeholder="Dinosaur's Name" 
-                    onChange={(e) => setName(e.target.value)} value={name}></input>
-                </div>
-                <div className="form-group mb-3">
-                    <label htmlFor="place" className="form-control dinoFormLabel">Enter your new dinosaur's size.</label>
-                    <input type="text" className="form-control dinoFormInput" id="dinoSize" placeholder="Dinosaur's Size"
-                    onChange={(e) => setSize(e.target.value)} value={size}></input>
-                </div>
-                <div className="form-group mb-3">
-                    <label htmlFor="place" className="form-control dinoFormLabel">Enter your new dinosaur's diet.</label>
-                    <input type="text" className="form-control dinoFormInput" id="dinoDiet" placeholder="Dinosaur's Diet"
-                    onChange={(e) => setDiet(e.target.value)} value={diet}></input>
-                </div>
-                <div className="form-group mb-3">
-                    <label htmlFor="place" className="form-control dinoFormLabel">Enter your new dinosaur's environment.</label>
-                    <input type="text" className="form-control dinoFormInput" id="dinoEnvironment" placeholder="Dinosaur's Environment"
-                    onChange={(e) => setEnvironment(e.target.value)} value={environment}></input>
-                </div>
-                <div>
-                    <button className="btn btn-primary form-control" id="addDino" type="submit">Add Dinosaur</button>
-                </div>
-            </form>
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (name && size && diet && environment) {
+      props.addNewDinosaur({ name, size, diet, environment });
+      setName("");
+      setSize("");
+      setDiet("");
+      setEnvironment("");
+      setImage("");
+      console.log("A new dinosaur is being created!");
+    } else {
+      console.log(
+        "Do not submit a blank value. Please enter a string for the dinosaur's name, size, diet, and environment."
+      );
+    }
+  };
+
+  return (
+    <div>
+      <form
+        className="container border border-success p-4"
+        id="dinoForm"
+        onSubmit={onSubmit}
+      >
+        <div className="form-group mb-3">
+          <label htmlFor="place" className="form-control dinoFormLabel">
+            Enter your new dinosaur name.
+          </label>
+          <input
+            type="text"
+            className="form-control dinoFormInput"
+            id="dinoName"
+            placeholder="Dinosaur's Name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          ></input>
         </div>
-    )
+        <div className="form-group mb-3">
+          <label htmlFor="place" className="form-control dinoFormLabel">
+            Enter your new dinosaur's size.
+          </label>
+          <input
+            type="text"
+            className="form-control dinoFormInput"
+            id="dinoSize"
+            placeholder="Dinosaur's Size"
+            onChange={(e) => setSize(e.target.value)}
+            value={size}
+          ></input>
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="place" className="form-control dinoFormLabel">
+            Enter your new dinosaur's diet.
+          </label>
+          <input
+            type="text"
+            className="form-control dinoFormInput"
+            id="dinoDiet"
+            placeholder="Dinosaur's Diet"
+            onChange={(e) => setDiet(e.target.value)}
+            value={diet}
+          ></input>
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="place" className="form-control dinoFormLabel">
+            Enter your new dinosaur's environment.
+          </label>
+          <input
+            type="text"
+            className="form-control dinoFormInput"
+            id="dinoEnvironment"
+            placeholder="Dinosaur's Environment"
+            onChange={(e) => setEnvironment(e.target.value)}
+            value={environment}
+          ></input>
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="place" className="form-control dinoFormLabel">
+            Choose your new dinosaur's image.
+          </label>
+          <select
+            className="form-control dinoFormInput"
+            id="dinoImage"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          >
+            <option value={TRex}>Tyrannosaurus Rex</option>
+            <option value="B">Banana</option>
+            <option value="C">Cranberry</option>
+          </select>
+        </div>
+        <div>
+          <button
+            className="btn btn-primary form-control"
+            id="addDino"
+            type="submit"
+          >
+            Add Dinosaur
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
